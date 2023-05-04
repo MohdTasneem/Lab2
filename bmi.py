ecce4babe43f397ff183f7b8e74f5186e7abb697
Tasneem <thajudeentasneem@gmail.com>
2023-05-04 11:13:01 +0800

    Changes of bmi.py for lab3

--- bmi.py
+++ bmi.py
@@ -1,13 +1,16 @@
-def calculate_bmi(height,weight):
+def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
     bmi = weight/(height*height)
     print("BMI = " + str(bmi))
-    if bmi<18.5:
+    if bmi < 18.5:
         print("You are Under Weight")
-    elif bmi>25:
+        return -1
+    elif bmi > 25:
         print("You are Over Weight")
+        return 1
     else:
         print("You are Normal Weight")
+        return 0
 
-calculate_bmi(weight=57,height=1.73)
+calculate_bmi(weight=57, height=1.73)
